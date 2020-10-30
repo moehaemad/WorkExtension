24969e435bfdfa493e8289a94c01fefbc03b319f
Muhammad <muhammad.ali.72.27@gmail.com>
2020-10-29 18:53:26 -0700

    removed unneeded permissions and handled empty website queries

--- menuFeatures.js
+++ menuFeatures.js
@@ -2,5 +2,7 @@ let button = document.getElementById('button');
 
 button.onclick = el =>{
     let input = document.getElementById('input').value;
-    chrome.tabs.create({active: true, url: `https://bestbuy.ca/en-ca/search?search=${input.split(' ').join('+')}`});
+    let url = ``
+    input === "" ? url = `https://www.bestbuy.ca/en-ca` : url = `https://bestbuy.ca/en-ca/search?search=${input.split(' ').join('+')}`
+    chrome.tabs.create({active: true, url: url});
 }
