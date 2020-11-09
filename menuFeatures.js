dc49074144d7b63e89c191f2d40964176f2a90b5
Muhammad <muhammad.ali.72.27@gmail.com>
2020-11-08 17:49:02 -0800

    added add ons and styling for ease-of-use

--- menuFeatures.js
+++ menuFeatures.js
@@ -6,3 +6,17 @@ button.onclick = el =>{
     input === "" ? url = `https://www.bestbuy.ca/en-ca` : url = `https://bestbuy.ca/en-ca/search?search=${input.split(' ').join('+')}`
     chrome.tabs.create({active: true, url: url});
 }
+
+document.querySelector('.Contacts').addEventListener('click', (e) => {
+    let insertOptions = `
+        <ol>
+            <li>Billing</li>
+            <li>In-home</li>
+            <li>Claims</li>
+            <li>Tech sup.</li>
+            <li>Assured Living</li>
+            <li>Other Q's</li>
+        </ol>
+    `
+    e.target.insertAdjacentHTML('afterend', insertOptions);
+})
